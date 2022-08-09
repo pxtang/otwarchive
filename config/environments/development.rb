5a762f0b54754c5ebcd94320847a89eefc1c9404
Peter Tang <petertangisme@gmail.com>
2022-08-08 20:30:04 -0700

    force memcache in dev, remove green banner

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,8 +4,11 @@ Otwarchive::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
-  config.cache_classes = false
-  config.eager_load = false
+  config.cache_classes = true
+  config.eager_load = true
+  # config.cache_classes = false
+  # config.eager_load = false
+
 
   memcached_servers = "127.0.0.1:11211"
   memcached_servers = YAML.load_file(Rails.root.join("config/local.yml")).fetch("MEMCACHED_SERVERS", memcached_servers) if File.file?(Rails.root.join("config/local.yml"))
@@ -20,7 +23,8 @@ Otwarchive::Application.configure do
 
   # Enable/disable caching. By default caching is disabled, but it can be
   # toggled on and off by calling rails dev:cache and restarting the server.
-  config.action_controller.perform_caching = Rails.root.join("tmp/caching-dev.txt").exist?
+  # config.action_controller.perform_caching = Rails.root.join("tmp/caching-dev.txt").exist?
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -31,17 +35,18 @@ Otwarchive::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Make it clear we are on Dev
-  config.rack_dev_mark.enable = true
+  # Don't make it clear we are on Dev. That's silly!
+  config.rack_dev_mark.enable = false
   config.rack_dev_mark.theme = [:title, Rack::DevMark::Theme::GithubForkRibbon.new(position: "left", color: "green", fixed: "true")]
 
-  config.eager_load = false
+  # why is this set twice?
+  # config.eager_load = false
   config.assets.enabled = false
   config.serve_static_files = true
 
-  # Enable Bullet gem to monitor application performance
+  # Don't Enable Bullet gem to monitor application performance
   config.after_initialize do
-    Bullet.enable = true
+    Bullet.enable = false
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.add_footer = false
