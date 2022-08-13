573cbc0331a1b2071cbdef9e373e3b80224a23d6
cherryblossom <clalin11@gmail.com>
2022-08-12 21:40:49 -0700

    rollback no fandoms tag, make it easier to disable memcache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,13 @@
 Otwarchive::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
+  memcache = false
+
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
-  config.cache_classes = true
-  config.eager_load = true
+  config.cache_classes = memcache
+  config.eager_load = memcache
   # config.cache_classes = false
   # config.eager_load = false
 
@@ -24,7 +26,7 @@ Otwarchive::Application.configure do
   # Enable/disable caching. By default caching is disabled, but it can be
   # toggled on and off by calling rails dev:cache and restarting the server.
   # config.action_controller.perform_caching = Rails.root.join("tmp/caching-dev.txt").exist?
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = memcache
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
